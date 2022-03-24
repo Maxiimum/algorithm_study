880c53b08f2cf09fc8768b176191fca550b15d5e
Maxiimum <80867166+Maxiimum@users.noreply.github.com>
2022-03-24 19:56:01 +0900

    Create leet_code_1108.js

---
+++ javascript/leet_code_1108.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+A defanged IP address replaces every period "." with "[.]".
+ 
+Example 1:
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+*/
+
+const defangIPaddr = function(address) {
+    const str = address.replace(/\./g, "[.]");
+    
+    return str;
+};
+
+console.log(defangIPaddr("1.1.1.1"));
+console.log(defangIPaddr("255.100.50.0"));
