aa049ff445c40b1aca910cb5a0924448edeeb2ad
Maxiimum <80867166+Maxiimum@users.noreply.github.com>
2022-04-04 23:55:21 +0900

    Create 서울에서김서방찾기.js

---
+++ javascript/서울에서김서방찾기.js
@@ -0,0 +1,12 @@
+function solution(seoul) {
+     let result = seoul.indexOf('Kim');
+
+    return `김서방은 ${result}에 있다`
+}
+
+
+function solution(seoul) {
+     let result = seoul.findIndex( a => a === 'Kim');
+    
+    return `김서방은 ${result}에 있다`
+}
